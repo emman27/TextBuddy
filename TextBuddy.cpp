ce74014c33902f85a21007e9ec8a5078deeadd3b
emman <eygohlolz@gmail.com>
2014-09-04 08:37:39 +0800

    Created constant FILE_NAME_POSTITION

--- TextBuddy.cpp
+++ TextBuddy.cpp
@@ -2,17 +2,20 @@
 
 #include "Program.h"
 
+
 int main(int argc, char* argv[])
 {
 	std::string filename;
 	std::vector<std::string> data;
 	FileControl fileCont;
-	const std::string FILE_ALREADY_SPECIFIED = "filealreadyspecified";
 	MessageSender print;
 	Program p;
 	
+	const std::string FILE_ALREADY_SPECIFIED = "filealreadyspecified";
+	const int FILE_NAME_POSITION = 1;
+	
 	filename = fileCont.checkFile(argc);
-	if (filename == FILE_ALREADY_SPECIFIED) filename = argv[1];
+	if (filename == FILE_ALREADY_SPECIFIED) filename = argv[FILE_NAME_POSITION];
 
 	data = fileCont.readFile(filename);
 
